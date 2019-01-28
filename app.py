f8771edc0e9f338d2827c6d865537518c5813cb6
Carlos <carlosriverosv@gmail.com>
2019-01-28 10:30:22 -0500

    Category table added. Search books in progress

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import os, hashlib, binascii
+import os
 from flask import Flask, jsonify, request, render_template
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
@@ -18,6 +18,19 @@ book_author = db.Table('book_author',
     db.Column('author_id', db.Integer, db.ForeignKey('author.id'), primary_key=True)
 )
 
+book_category = db.Table('book_category',
+    db.Column('book_id', db.Integer, db.ForeignKey('book.id'), primary_key=True),
+    db.Column('category_id', db.Integer, db.ForeignKey('category.id'), primary_key=True)
+)
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True)
+
+    def __repr__(self):
+        return '<Category, {}>'.format(self.name)
+
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -27,6 +40,7 @@ class Book(db.Model):
     description = db.Column(db.String(200), unique=False, nullable=True)
     url_image = db.Column(db.String(80), unique=False, nullable=True)
     authors = db.relationship('Author', secondary=book_author, lazy='subquery', backref=db.backref('books', lazy=True))
+    categories = db.relationship('Category', secondary=book_category, lazy='subquery', backref=db.backref('books', lazy=True))
 
     def __repr__(self):
         return '<Book,{}>'.format(self.title)
@@ -34,10 +48,29 @@ class Book(db.Model):
 
 class Author(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=False, nullable=True)
+    name = db.Column(db.String(80), unique=True)
 
     def __repr__(self):
         return '<Author,{}>'.format(self.name)
+
+
+@app.route('/categories', methods=['GET', 'POST', 'DELETE'])
+def categories():
+    if request.method == 'POST':
+        data = request.json
+        category = Category(name=data.get('name'))
+        try:
+            db.session.add(category)
+            db.session.commit()
+        except IntegrityError as e:
+            print(e)
+            return jsonify({"error": {"description": "Category already exist"}}), 400
+        else:
+            return jsonify({"data": {"name": category.name}}), 201
+    elif request.method == 'GET':
+        categories_ = Category.query.all()
+        result = [{"name": category.name} for category in categories_]
+        return jsonify({"data": result}), 200
 
 
 @app.route('/authors', methods=['GET', 'POST', 'DELETE'])
@@ -64,11 +97,16 @@ def books():
     if request.method == "POST":
         data = request.json
         auth = data.get('authors')
+        cat = data.get('categories')
         book = Book(title=data.get('title'), subtitle=data.get('subtitle'))
         for author in auth:
             au = Author.query.filter_by(name=author).first()
             if au:
                 book.authors.append(au)
+        for c in cat:
+            category_ = Category.query.filter_by(name=c).first()
+            if category_:
+                book.categories.append(category_)
         try:
             db.session.add(book)
             db.session.commit()
@@ -97,6 +135,12 @@ def books():
             return jsonify({"error": {"description": "Book does not exist"}}), 400
 
 
+@app.route('/books/search', methods=['GET', 'POST', 'DELETE'])
+def search_books():
+    title = request.args.get('title')
+    subtitle = request.args.get('subtitle')
+    editor = request.args.get('editor')
+    
 
 db.create_all()
 
